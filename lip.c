107f0ea835c60016d8aaf7bff609809296115595
Julio Jimenez <julio@julioj.com>
2023-11-17 09:56:50 -0500

    oh yeah
    Signed-off-by: Julio Jimenez <julio@julioj.com>

--- lip.c
+++ lip.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define VERSION "0.0.2"
+#define VERSION "0.0.3"
 
 // if we are compiling on windows compile these functions
 #ifdef _WIN32
@@ -29,6 +29,44 @@ void add_history(char* unused) {}
 #else
 #include <editline/readline.h>
 #endif
+
+int number_of_nodes(mpc_ast_t* t) {
+  if (t->children_num == 0) { return 1; }
+  if (t->children_num >= 1) {
+    int total = 1;
+    for (int i = 0; i < t->children_num; i++) {
+      total = total + number_of_nodes(t->children[i]);
+    }
+    return total;
+  }
+  return 0;
+}
+
+long eval(mpc_ast_t* t) {
+  // If tagged as number return it directly
+  if (strstr(t->tag, "number")) {
+    return atoi(t->contents);
+  }
+  // The operator is always second child
+  char* op = t->children[1]->contents;
+  // We store the third child in x
+  long x = eval(t->children[2]);
+  // Iterate the remaining children and combining
+  int i = 3;
+  while (strstr(t->children[i]->tag, "expr")) {
+    x = eval_op(x, op, eval(t->children[i]));
+    i++;
+  }
+  return x;
+}
+
+// Use operator string to see which operation to perform
+long eval_op(long x, char* op, long y) {
+  if (strcmp(op, "+") == 0) { return x + y; }
+  if (strcmp(op, "-") == 0) { return x - y; }
+  if (strcmp(op, "*") == 0) { return x * y; }
+  if (strcmp(op, "/") == 0) { return x / y; }
+}
 
 int main(int argc, char** argv) {
   // Create some parsers
@@ -60,8 +98,19 @@ int main(int argc, char** argv) {
     // Attempt to parse the user input
     mpc_result_t r;
     if (mpc_parse("<stdin>", input, Lip, &r)) {
-      // On success print and delete the AST
+      // Print AST
       mpc_ast_print(r.output);
+      // Load AST from output
+      mpc_ast_t* a = r.output;
+      printf("Tag: %s\n", a->tag);
+      printf("Contents: %s\n", a->contents);
+      printf("Number of children: %i\n", a->children_num);
+      // Get first child
+      mpc_ast_t* c0 = a->children[1];
+      printf("First Child Tag: %s\n", c0->tag);
+      printf("First Child Contents: %s\n", c0->contents);
+      printf("First Child Number of Children: %i\n", c0->children_num);
+      // Delete AST
       mpc_ast_delete(r.output);
     } else {
       // Otherwise print and delete the error
